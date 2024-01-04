a9b1e24a635bc2ae609517779732f4d9796cb321
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-01-05 00:35:19 +0800

    chore: use tsup

---
+++ tsup.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'tsup'
+
+export default defineConfig({
+  name: 'tsup',
+  target: 'node14',
+  format: ['cjs', 'esm'],
+  splitting: false,
+  sourcemap: false,
+  clean: true,
+  dts: true,
+  entryPoints: ['src/index.ts', 'src/index.css'],
+  define: {
+    __DEV__: 'false',
+  },
+  bundle: true,
+  external: [
+    'vite',
+    'vue',
+    'vue/compiler-sfc',
+    'webpack',
+    'rollup',
+    'esbuild',
+    'tsup',
+    'spark-md5',
+    'qrcode',
+    'htmlparser2',
+    '@vueuse/core',
+  ],
+  platform: 'node',
+})
